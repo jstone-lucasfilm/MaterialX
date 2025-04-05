ab4b83a3a01d0bf3d1a4107b102a0f5c98f1743e
Jonathan Stone <jstone@lucasfilm.com>
2025-04-04 20:45:50 -0700

    Fix GLSL implementation of BSDF addition
    This changelist fixes the computation of throughput in the GLSL implementation of BSDF addition, with a comment describing the motivation for the provided math.

--- libraries/pbrlib/genglsl/mx_add_bsdf.glsl
+++ libraries/pbrlib/genglsl/mx_add_bsdf.glsl
@@ -2,6 +2,8 @@
 
 void mx_add_bsdf(ClosureData closureData, BSDF in1, BSDF in2, out BSDF result)
 {
+    // In GLSL, we interpret closure addition as mix(in1, in2, 0.5) * 2, which
+    // gives us the following math for the response and throughput.
     result.response = in1.response + in2.response;
-    result.throughput = in1.throughput + in2.throughput;
+    result.throughput = mix(in1.throughput, in2.throughput, 0.5);
 }
