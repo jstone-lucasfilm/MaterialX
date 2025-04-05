c35a9a7d7ecf34d8b5467a9b921965e4c12974fb
Jonathan Stone <stonej212@gmail.com>
2025-04-05 09:11:10 -0700

    Update mx_add_bsdf.glsl

--- libraries/pbrlib/genglsl/mx_add_bsdf.glsl
+++ libraries/pbrlib/genglsl/mx_add_bsdf.glsl
@@ -2,9 +2,9 @@
 
 void mx_add_bsdf(ClosureData closureData, BSDF in1, BSDF in2, out BSDF result)
 {
-    // Hardware shading languages require both response and throughput values,
-    // so we obtain the following logic by refactoring closure addition as
-    // in1 + in2 = mix(in1, in2, 0.5) * 2.
+    // In hardware shading languages, closure operators require both response
+    // and throughput computations, so we obtain the following logic by
+    // refactoring closure addition as in1 + in2 = mix(in1, in2, 0.5) * 2.
     result.response = in1.response + in2.response;
     result.throughput = mix(in1.throughput, in2.throughput, 0.5);
 }
